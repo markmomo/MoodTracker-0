cbde3d85e40099d67893f1fded49ec273510d454
markmomo <markmomo.mm@gmail.com>
2019-03-05 23:19:43 +0100

    In MainActivity : add displayNoteBox() add OnClick()

--- app/src/main/java/markmomo/com/myamazingviewpagertraining/controllers/MainActivity.java
+++ app/src/main/java/markmomo/com/myamazingviewpagertraining/controllers/MainActivity.java
@@ -1,16 +1,28 @@
 package markmomo.com.myamazingviewpagertraining.controllers;
 
 
+import android.content.DialogInterface;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import markmomo.com.myamazingviewpagertraining.adapters.PageAdapter;
 import markmomo.com.myamazingviewpagertraining.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageButton noteButtonIcon ;
     private ImageButton historyButtonIcon;
+
+    @Override
+    public void onClick(View v) {
+        int buttonIndex;
+        buttonIndex = (int)v.getTag();
+
+        if (buttonIndex == 1) displayNoteBox();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
 
         noteButtonIcon = findViewById(R.id.activity_main_note_icon);
         historyButtonIcon = findViewById(R.id.activity_main_history_icon);
+        noteButtonIcon.setOnClickListener(this);
+        historyButtonIcon.setOnClickListener(this);
+        noteButtonIcon.setTag(1);
+        historyButtonIcon.setTag(2);
 
         this.configureViewPager();
     }
@@ -36,4 +52,31 @@ public class MainActivity extends AppCompatActivity {
         noteButtonIcon.setBackgroundColor(pageAdapter1.getIconColor());
         historyButtonIcon.setBackgroundColor(pageAdapter1.getIconColor());
     }
+    private void displayNoteBox(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Commentaire");
+        alert.setMessage("Commentaire");
+
+        // Set an EditText view to get user input
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+        alert.setPositiveButton("Valider", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Do something with value!
+            }
+        });
+        alert.setCancelable(false);
+        alert.create();
+        alert.show();
+
+    }
+
+
 }
