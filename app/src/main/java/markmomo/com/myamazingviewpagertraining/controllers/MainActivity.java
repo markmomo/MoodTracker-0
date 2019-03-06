3e6446794fdebb99757294947e1be8eefd504a22
markmomo <markmomo.mm@gmail.com>
2019-03-06 19:56:12 +0100

    Adding methods myStoreCommentOfTheMood() on MainActivity

--- app/src/main/java/markmomo/com/myamazingviewpagertraining/controllers/MainActivity.java
+++ app/src/main/java/markmomo/com/myamazingviewpagertraining/controllers/MainActivity.java
@@ -2,11 +2,13 @@ package markmomo.com.myamazingviewpagertraining.controllers;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import markmomo.com.myamazingviewpagertraining.adapters.PageAdapter;
@@ -15,6 +17,15 @@ import markmomo.com.myamazingviewpagertraining.R;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageButton noteButtonIcon ;
     private ImageButton historyButtonIcon;
+    ViewPager viewPager;
+    SharedPreferences mPreferences;
+
+    private EditText mEditTextBox;
+    public static final String PREF_KEY_SAD_COMMENT = "PREF_KEY_SAD_COMMENT";
+    public static final String PREF_KEY_DISAPPOINTED_COMMENT = "PREF_KEY_DISAPPOINTED_COMMENT";
+    public static final String PREF_KEY_NORMAL_COMMENT = "PREF_KEY_NORMAL_COMMENT";
+    public static final String PREF_KEY_HAPPY_COMMENT = "PREF_KEY_HAPPY_COMMENT";
+    public static final String PREF_KEY_SUPER_HAPPY_COMMENT = "PREF_KEY_SUPER_HAPPY_COMMENT";
 
     @Override
     public void onClick(View v) {
@@ -35,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEditTextBox = new EditText(this);
         noteButtonIcon = findViewById(R.id.activity_main_note_icon_button);
         historyButtonIcon = findViewById(R.id.activity_main_history_icon_button);
         noteButtonIcon.setOnClickListener(this);
@@ -42,11 +54,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         noteButtonIcon.setTag(1);
         historyButtonIcon.setTag(2);
 
+        System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_SAD_COMMENT, null));
+        System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_DISAPPOINTED_COMMENT, null));
+        System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_NORMAL_COMMENT, null));
+        System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_HAPPY_COMMENT, null));
+        System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_SUPER_HAPPY_COMMENT, null));
+
         this.configureViewPager();
     }
 
     private void configureViewPager(){
-        ViewPager viewPager;
+
         PageAdapter pageAdapter1;
 
         pageAdapter1 = new PageAdapter(getSupportFragmentManager(), getResources().getIntArray(R.array.colorPagesViewPager));
@@ -59,11 +77,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         historyButtonIcon.setBackgroundColor(pageAdapter1.getMainIconsColor());
     }
     private void myDisplayNoteBox(){
-        final EditText input = new EditText(this);
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
         alert.setTitle("Commentaire");
-        alert.setView(input);
+        alert.setView(mEditTextBox);
 
         alert.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
@@ -73,10 +90,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         alert.setPositiveButton("Valider", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
 
+                myStoreCommentOfTheMood();
+
+                System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_SAD_COMMENT, null));
+                System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_DISAPPOINTED_COMMENT, null));
+                System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_NORMAL_COMMENT, null));
+                System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_HAPPY_COMMENT, null));
+                System.out.println(getPreferences(MODE_PRIVATE).getString(PREF_KEY_SUPER_HAPPY_COMMENT, null));
             }
         });
         alert.setCancelable(false);
         alert.create();
+        if(mEditTextBox.getParent() != null) {
+            ((ViewGroup)mEditTextBox.getParent()).removeView(mEditTextBox); // <- fix
+        }
         alert.show();
+    }
+
+
+
+
+
+    private void myStoreCommentOfTheMood(){
+
+        switch (viewPager.getCurrentItem()){
+            case 0:
+                mPreferences = getPreferences(MODE_PRIVATE);
+                mPreferences.edit().putString(PREF_KEY_SAD_COMMENT, mEditTextBox.getText().toString()).apply();
+                break;
+            case 1:
+                mPreferences = getPreferences(MODE_PRIVATE);
+                mPreferences.edit().putString(PREF_KEY_DISAPPOINTED_COMMENT, mEditTextBox.getText().toString()).apply();
+                break;
+            case 2:
+                mPreferences = getPreferences(MODE_PRIVATE);
+                mPreferences.edit().putString(PREF_KEY_NORMAL_COMMENT, mEditTextBox.getText().toString()).apply();
+                break;
+            case 3:
+                mPreferences = getPreferences(MODE_PRIVATE);
+                mPreferences.edit().putString(PREF_KEY_HAPPY_COMMENT, mEditTextBox.getText().toString()).apply();
+                break;
+            case 4:
+                mPreferences = getPreferences(MODE_PRIVATE);
+                mPreferences.edit().putString(PREF_KEY_SUPER_HAPPY_COMMENT, mEditTextBox.getText().toString()).apply();
+                break;
+        }
     }
 }
